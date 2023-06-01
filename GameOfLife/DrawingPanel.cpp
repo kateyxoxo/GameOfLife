302180ed7a96fe696f13694afd6eae23780ff095
Katelyn Sages <KNSages@student.fullsail.edu>
2023-06-01 17:57:39 -0400

    Completed Grid Construction part 1.

--- GameOfLife/DrawingPanel.cpp
+++ GameOfLife/DrawingPanel.cpp
@@ -5,6 +5,8 @@ DrawingPanel::DrawingPanel(wxFrame* parent) :wxPanel(parent, wxID_ANY)
 {
 	this->SetBackgroundStyle(wxBG_STYLE_PAINT);
 	this->Bind(wxEVT_PAINT, &DrawingPanel::OnPaint, this);
+	gridSize = 15;
+	cellSize = 10;
 };
 DrawingPanel::~DrawingPanel()
 {
@@ -21,5 +23,23 @@ void DrawingPanel::OnPaint(wxPaintEvent& wxPE)
 	}
 	context->SetPen(*wxBLACK_PEN);
 	context->SetBrush(*wxWHITE_BRUSH);
-	context->DrawRectangle(100, 100, 100, 100);
+	//context->DrawRectangle(100, 100, 100, 100);
+	int panelWidth, panelHeight;
+	GetSize(&panelWidth, &panelHeight);
+
+	int cellWidth = panelWidth / gridSize;
+	int cellHeight = panelHeight / gridSize;
+
+	for (int i = 0; i < gridSize; i++)
+	{
+		for (int j = 0; j < gridSize; j++)
+		{
+			int x = i * cellWidth;
+			int y = j * cellHeight;
+
+			context->DrawRectangle(x, y, cellWidth, cellHeight);
+		}
+	}
+
+	delete context;
 }
--- GameOfLife/DrawingPanel.h
+++ GameOfLife/DrawingPanel.h
@@ -6,7 +6,11 @@ class DrawingPanel :
 public:
         DrawingPanel(wxFrame*);
         ~DrawingPanel();
+private:
         void OnPaint(wxPaintEvent&);
+        int gridSize;
+        int cellSize;
+
 };
 
 
